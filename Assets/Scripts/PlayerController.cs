c2ce96e7e66d7b60d7ee04d4b2ca1363b74d21c7
LieRossi <40723203+LieRossi@users.noreply.github.com>
2018-12-16 09:40:42 -0200

    ChaoAbrindo

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,110 +4,111 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour {
 
-	public float MoveSpeed;
-	public float RotationSpeed;
-	CharacterController cc;
-	private Animator anim;
-	protected Vector3 gravidade = Vector3.zero;
-	protected Vector3 move = Vector3.zero;
-	private bool jump = false;
+    public float MoveSpeed;
+    public float RotationSpeed;
+    CharacterController cc;
+    private Animator anim;
+    protected Vector3 gravidade = Vector3.zero;
+    protected Vector3 move = Vector3.zero;
+    private bool jump = false;
 
     public int valPedraAzul = 0;
     public int valPedraVerde = 0;
-    private bool Aberto ;
-    private bool porta ;
+    private bool Aberto;
+    private bool porta;
 
 
 
     void Start()
-	{
-		cc = GetComponent<CharacterController> ();
-		anim = GetComponent<Animator>();
-		anim.SetTrigger("Parado");
-	}
+    {
+        cc = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        anim.SetTrigger("Parado");
+    }
 
-	void Update()
-	{
-		Vector3 move = Input.GetAxis ("Vertical") * transform.TransformDirection (Vector3.forward) * MoveSpeed;
-		transform.Rotate (new Vector3 (0, Input.GetAxis ("Horizontal") * RotationSpeed * Time.deltaTime, 0));
-		
-		if (!cc.isGrounded) {
-			gravidade += Physics.gravity * Time.deltaTime;
-		} 
-		else 
-		{
-			gravidade = Vector3.zero;
-			if(jump)
-			{
-				gravidade.y = 6f;
-				jump = false;
-			}
-		}
-		move += gravidade;
-		cc.Move (move* Time.deltaTime);
-		Anima ();
-	}
-	 
-	void Anima()
-	{
-		if (!Input.anyKey)
-		{
-			anim.SetTrigger("Parado");
-		} 
-		else 
-		{
-			if(Input.GetKeyDown("space"))
-			{
-				anim.SetTrigger("Pula");
-				jump = true;
-			}
-			else
-			{
-				anim.SetTrigger("Corre");
-			}
-		}
-	}
+    void Update()
+    {
+        Vector3 move = Input.GetAxis("Vertical") * transform.TransformDirection(Vector3.forward) * MoveSpeed;
+        transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * RotationSpeed * Time.deltaTime, 0));
+
+        if (!cc.isGrounded) {
+            gravidade += Physics.gravity * Time.deltaTime;
+        }
+        else
+        {
+            gravidade = Vector3.zero;
+            if (jump)
+            {
+                gravidade.y = 6f;
+                jump = false;
+            }
+        }
+        move += gravidade;
+        cc.Move(move * Time.deltaTime);
+        Anima();
+    }
+
+    void Anima()
+    {
+        if (!Input.anyKey)
+        {
+            anim.SetTrigger("Parado");
+        }
+        else
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                anim.SetTrigger("Pula");
+                jump = true;
+            }
+            else
+            {
+                anim.SetTrigger("Corre");
+            }
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("pedraAzul")){ 
-        
+        if (other.gameObject.CompareTag("pedraAzul"))
+        {
             valPedraAzul++;
             AbrePorta.ScoreA++;
-            Debug.Log(valPedraAzul);
-
+            //Debug.Log(valPedraAzul);
         }
 
 
-        if (other.gameObject.CompareTag("pedraVerde")){ 
-        
+        if (other.gameObject.CompareTag("pedraVerde"))
+        {
             valPedraVerde++;
             AbrePorta.ScoreV++;
-            Debug.Log(valPedraVerde);
+            //Debug.Log(valPedraVerde);
+        }
+         if (other.gameObject.CompareTag("Bomb"))
+        {
+            Destroy(gameObject);
         }
 
-        //if (valPedraAzul >= 1 && valPedraVerde >= 1)
-       // {
-           // porta = true;
-            //other.GetComponent<AbrePorta>().Abrindo();
-            //anim.SetTrigger("Aberto");
-           // Aberto = true; 
-       // }
+        /* if (other.gameObject.CompareTag("Boxparede"))
+         {
+             Destroy(gameObject);
+         }*/
+    }
+
+   /* private void OnControllerColliderHit(ControllerColliderHit other)
+    {
+            if (other.gameObject.CompareTag("Bomb"))
+            {
+                Destroy(gameObject);
+            }*/
 
 
+            /*if(other.gameObject.CompareTag("Espinhos"))//|| other.gameObject.CompareTag("Estalac")||other.gameObject.CompareTag("Lava"))
+            {
+                    Destroy(gameObject);
+            }
 
-
-            /* if (other.gameObject.CompareTag("Parede"))
-             {
-                 Destroy(gameObject);
-             }
-
-
-              if(other.gameObject.CompareTag("Espinhos")|| other.gameObject.CompareTag("Estalac")||other.gameObject.CompareTag("Lava"))
-             {
-                 Destroy(gameObject);
-             }*/
-        }
+    */
 
 
 }
