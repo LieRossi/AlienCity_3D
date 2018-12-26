4bda6a55cd5342be73e799f14baf25331fb7bdd0
LieRossi <40723203+LieRossi@users.noreply.github.com>
2018-12-26 19:13:00 -0200

    assets Chaves

---
+++ Assets/Scripts/RotacionaChave.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionaChave : MonoBehaviour {
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 1, 0), 30 * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") || (other.gameObject.CompareTag("tiro")))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
